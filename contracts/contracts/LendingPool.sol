1a2205386f95a8769911b1ae673c9e3314a843c1
Rcontre360 <rcontreraspimentel@gmail.com>
2022-05-08 13:24:34 -0400

    feat(contracts utils/): lending pool as superApp. Test utils
    modified lending pool as superfluid app. CReated shares token. Created utils for testing and scripts
    
    BREAKING CHANGE: n

--- contracts/contracts/LendingPool.sol
+++ contracts/contracts/LendingPool.sol
@@ -2,23 +2,43 @@
 pragma solidity ^0.8.13;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ContextUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
-import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { Context } from "@openzeppelin/contracts/utils/Context.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-import { OwnableInitializable } from "./access/OwnableInitializable.sol";
+import { ISuperfluid, ISuperToken, ISuperApp, ISuperAgreement, SuperAppDefinitions } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { CFAv1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import { IConstantFlowAgreementV1 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import { SuperAppBase } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+
+import { IMintableSuperToken } from "./interfaces/IMintableSuperToken.sol";
 import { ILendingPool } from "./interfaces/ILendingPool.sol";
 import { IERC20Decimals } from "./interfaces/IERC20Decimals.sol";
 
-contract LendingPool is ILendingPool, ContextUpgradeable, OwnableInitializable, ERC20Upgradeable {
-    using SafeERC20 for IERC20Decimals;
+contract LendingPool is ILendingPool, Context, Ownable, SuperAppBase {
+    using SafeERC20 for IMintableSuperToken;
+    using CFAv1Library for CFAv1Library.InitData;
 
-    IERC20Decimals public token;
+    CFAv1Library.InitData public cfaV1;
+    ISuperfluid public host;
+    IConstantFlowAgreementV1 public cfa;
 
-    constructor(IERC20Decimals _token) {}
+    IMintableSuperToken public token;
+    IMintableSuperToken public sharesToken;
 
-    function initialize(IERC20Decimals _token) external initialize {
+    constructor(IMintableSuperToken _token, ISuperfluid _host) {
         token = _token;
-        ERC20Upgradeable.initialize("Lending Pool token", "LP");
+        host = _host;
+        cfa = IConstantFlowAgreementV1(
+            address(host.getAgreementClass(keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")))
+        );
+        cfaV1 = CFAv1Library.InitData(host, cfa);
+        uint256 configWord = SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+
+        _host.registerApp(configWord);
     }
 
     function deposit(uint256 amount) external {
@@ -27,7 +47,7 @@ contract LendingPool is ILendingPool, ContextUpgradeable, OwnableInitializable, 
         uint256 shares = sharesGivenAmount(amount);
 
         token.safeTransferFrom(sender, address(this), amount);
-        _mint(sender, shares);
+        sharesToken.mint(sender, shares, "");
 
         emit Deposit(sender, amount, shares);
     }
@@ -35,23 +55,18 @@ contract LendingPool is ILendingPool, ContextUpgradeable, OwnableInitializable, 
     function withdraw(uint256 shares) external {
         address sender = _msgSender();
         uint256 funds = token.balanceOf(address(this));
-        uint256 finalWithdraw = (shares * funds) / totalSupply();
+        uint256 finalWithdraw = (shares * funds) / sharesToken.totalSupply();
 
-        _burn(sender, shares);
+        sharesToken.burn(sender, shares, "");
         token.safeTransfer(sender, finalWithdraw);
 
         emit Withdraw(sender, shares, finalWithdraw);
     }
 
-    function forward(address[] memory targets, uint256[] memory amounts) external onlyOwner {
-        require(targets.length == amounts.length, "LendingPool:Input length mismatch");
-        for (uint256 i = 0; i < targets.length; i++) _forward(targets[i], amounts[i]);
-    }
-
     function sharesGivenAmount(uint256 amount) public view returns (uint256) {
-        uint256 _totalSupply = totalSupply();
+        uint256 _totalSupply = sharesToken.totalSupply();
         if (_totalSupply == 0) {
-            return (amount * 10**decimals()) / (10**token.decimals());
+            return (amount * 10**sharesToken.decimals()) / (10**token.decimals());
         } else {
             return (amount * _totalSupply) / value();
         }
@@ -69,5 +84,42 @@ contract LendingPool is ILendingPool, ContextUpgradeable, OwnableInitializable, 
     function _forward(address to, uint256 amount) internal {
         token.transfer(to, amount);
         emit Forwarded(to, amount);
+    }
+
+    function _updateOutflow(bytes calldata ctx) private returns (bytes memory newCtx) {
+        newCtx = ctx;
+    }
+
+    function afterAgreementCreated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata, /*_agreementData*/
+        bytes calldata, // _cbdata,
+        bytes calldata ctx
+    ) external override returns (bytes memory newCtx) {
+        newCtx = _updateOutflow(ctx);
+    }
+
+    function afterAgreementUpdated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata, /*_agreementData*/
+        bytes calldata, // _cbdata,
+        bytes calldata ctx
+    ) external override returns (bytes memory newCtx) {
+        newCtx = _updateOutflow(ctx);
+    }
+
+    function afterAgreementTerminated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata, /*_agreementData*/
+        bytes calldata, // _cbdata,
+        bytes calldata ctx
+    ) external override returns (bytes memory newCtx) {
+        newCtx = _updateOutflow(ctx);
     }
 }
