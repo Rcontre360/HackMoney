6032fff69a7255525dd12dfe081e1930a55dd528
sushmit_windows <sushmitsarmah@gmail.com>
2022-05-22 12:47:58 +0530

    create dao background fixed

--- client/pages/createdao.tsx
+++ client/pages/createdao.tsx
@@ -1,8 +1,15 @@
 import Borrow from "@shared/components/borrow";
+import LayoutLanding from "@shared/layout/layoutLanding";
 import React from "react";
 
 const CreateDao: React.FunctionComponent<{}> = (props) => {
-  return <Borrow />;
+  return (
+    <LayoutLanding>
+      <div className="flex flex-col items-center bg-color2">
+        <Borrow />
+      </div>
+    </LayoutLanding>
+  );
 };
 
 export default CreateDao;
